cc12cf55ccd588d17b239e57e9838a93e17ab2f0
Rahim Charania <rahimkcharania@gmail.com>
2019-07-01 17:12:20 -0500

    Update email.sh

--- email_helpdesk/email.sh
+++ email_helpdesk/email.sh
@@ -3,4 +3,4 @@ echo `date` >> /gpfs/scratch/rkc10/emaillogs.txt
 echo "" >> /gpfs/scratch/rkc10/emaillogs.txt
 cat /gpfs/scratch/rkc10/highloadavg.csv >> /gpfs/scratch/rkc10/emaillogs.txt
 
-/usr/sbin/sendmail -f rkc10@psu.edu rkc10@psu.edu < /gpfs/scratch/rkc10/emaillogs.txt
+/usr/sbin/sendmail -f myemail helpdeskemail < /gpfs/scratch/rkc10/emaillogs.txt
